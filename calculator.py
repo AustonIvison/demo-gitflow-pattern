12595b5728b126e526b0b46d90f1fb16f9b59d9b
Auston Ivison <austonivison@users.noreply.github.com>
2025-12-19 16:18:32 -0700

    Feat: Add multiplication (Gitflow: Feature branch work)

--- calculator.py
+++ calculator.py
@@ -5,6 +5,9 @@ class Calculator:
     def subtract(self, a, b):
         return a - b
 
+    def multiply(self, a, b):
+        return a * b
+
 if __name__ == "__main__":
     calc = Calculator()
     print("1 + 2 =", calc.add(1, 2))
